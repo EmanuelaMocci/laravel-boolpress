b4f1a27460d08754d595961cd67ae64fc141d575
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-11-23 15:34:57 +0100

    Aggiunta vue

---
+++ resources/js/components/Main.vue
@@ -0,0 +1,29 @@
+<template>
+    <main>
+        <h1>Main</h1>
+        <ul>
+            <li v-for="post in posts" :key="post.id">{{post.title}}</li>
+        </ul>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: "Main",
+        data() {
+            return {
+                posts: []
+            }
+        },
+        mounted(){
+            axios.get('/api/posts')
+            .then((response) => {
+                this.posts = response.data.data;
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        }
+    }
+</script>
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', 'PageController@index');
 Route::get('/blog', 'PostController@index')->name("posts.index");
 Route::get('/blog/{slug}', 'PostController@show')->name("posts.show");
 Route::get('/blog/category/{slug}', 'CategoryController@show')->name("categories.show");
-
+Route::get('/api-posts', 'PageController@apiPosts')->name("posts.api");
 
 // Rotte Autenticazione
 Auth::routes();
